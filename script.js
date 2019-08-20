c5144337215deda0a9526c47f5dc6c8094d141ba
Nihir Agarwal <f20180701@pilani.bits-pilani.ac.in>
2019-08-20 15:13:18 +0530

    finished

---
+++ script.js
@@ -0,0 +1,91 @@
+// DOM Declartions
+const newgridButton = document.querySelector("#newgrid");
+const clearButton = document.querySelector('#clear');
+const gridContainer = document.querySelector('.grid-container');
+const chooseColorButton = document.querySelector('.options input');
+const randomcolorButton = document.querySelector('#randomcolor');
+
+
+//ColorMode Variable Declaration
+let colorMode = "custom";
+
+
+//RandomColor
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+
+
+
+//Grid Generating Function
+function newGrid()
+{
+    while(gridContainer.firstChild)
+        gridContainer.removeChild(gridContainer.firstChild);
+    do
+    {
+        var gridSize = parseInt(window.prompt("Please enter the number of squares you want on one side of the grid ?","1"),10);
+
+    }
+    while(isNaN(gridSize));
+
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
+    for(let i=0;i<gridSize*gridSize;i++)
+    {
+        var newSquare = document.createElement('div');
+        newSquare.classList.add("grid");
+        gridContainer.appendChild(newSquare);
+    }
+    for(let i=0;i<gridContainer.children.length;i++)
+    {
+        gridContainer.children[i].addEventListener('mouseover',(e)=>{
+            // console.log(e.target.style.background);
+            let color;
+            if(colorMode =="random")
+            {
+                color = getRandomColor();
+            }
+            else
+            {
+                color = chooseColorButton.value;
+            }
+            e.target.style.background = color;
+        })
+    }
+}
+
+
+
+// Clear function
+function clear()
+{
+    while(gridContainer.firstChild)
+    gridContainer.removeChild(gridContainer.firstChild);
+}
+
+
+  
+//SetDefaultColor
+// chooseColorButton.defaultValue = "#8ed081";
+
+
+// Event Listeners
+newgridButton.addEventListener('click',newGrid);
+
+clearButton.addEventListener('click',clear);
+
+randomcolorButton.addEventListener('click', function() {
+    colorMode = "random";
+})
+chooseColorButton.addEventListener('click', function() {
+    colorMode = "custom";
+})
